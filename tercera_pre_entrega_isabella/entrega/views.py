df0097bc6b17bd9efd4554be496e7d12cfc636ed
juanhu9999 <juanpablo.isabella@gmail.com>
2024-08-06 18:04:40 -0300

    update ultimo momento

--- tercera_pre_entrega_isabella/entrega/views.py
+++ tercera_pre_entrega_isabella/entrega/views.py
@@ -17,7 +17,7 @@ def form_ingresar_datos_autos(request):
 
     if request.method == 'POST':
 
-        ingresar_datos = Autos(modelo=request.POST ['modelos'], matricula=request.POST ['matricula'])
+        ingresar_datos = Autos(modelo=request.POST ['modelo'], matricula=request.POST ['matricula'])
 
         ingresar_datos.save()
 
@@ -37,3 +37,11 @@ def form_ingresar_datos_motos(request):
         ingresar_datos.save()
         return render(request, "entrega/inicio.html")
     return render(request, "entrega/ingresar_datos.html")
+
+def buscar_datos(request):
+    query = request.GET.get('q', '')
+    resultados_autos = Autos.objects.filter(modelo__icontains=query)
+    resultados_camiones = Camiones.objects.filter(modelo__icontains=query)
+    resultados_motos = Motos.objects.filter(modelo__icontains=query)
+    # Haz algo con los resultados aquí, como pasarlos a tu plantilla para mostrarlos
+    return render(request, "entrega/buscar_datos.html", {'resultados_autos': resultados_autos, 'resultados_camiones': resultados_camiones, 'resultados_motos': resultados_motos})
