4a6a7b47bfa0811dbaef0d73ae53274023f67420
juanhu9999 <juanpablo.isabella@gmail.com>
2024-08-06 17:05:02 -0300

    update importante

--- tercera_pre_entrega_isabella/entrega/urls.py
+++ tercera_pre_entrega_isabella/entrega/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from entrega.views import inicio, ingresar_datos, buscar_datos
+from entrega.views import inicio, ingresar_datos, buscar_datos, form_ingresar_datos_autos, form_ingresar_datos_camiones, form_ingresar_datos_motos
+
+from django.urls import path
+from entrega.views import inicio, ingresar_datos, buscar_datos, form_ingresar_datos_autos, form_ingresar_datos_camiones, form_ingresar_datos_motos
 
 urlpatterns = [
     path('', inicio, name="inicio"),
     path('ingresar_datos', ingresar_datos, name="ingresar_datos"),
     path('buscar_datos', buscar_datos, name="buscar_datos"),
-
+    path('form_ingresar_datos_autos', form_ingresar_datos_autos, name="form_ingresar_datos_autos"),
+    path('form_ingresar_datos_camiones', form_ingresar_datos_camiones, name="form_ingresar_datos_camiones"),
+    path('form_ingresar_datos_motos', form_ingresar_datos_motos, name="form_ingresar_datos_motos"),
 ]
--- tercera_pre_entrega_isabella/entrega/views.py
+++ tercera_pre_entrega_isabella/entrega/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from entrega.models import Autos, Camiones, Motos
 
 # Create your views here.
 
@@ -11,3 +12,28 @@ def ingresar_datos(request):
 
 def buscar_datos(request):
     return render(request, "entrega/buscar_datos.html")
+
+def form_ingresar_datos_autos(request):
+
+    if request.method == 'POST':
+
+        ingresar_datos = Autos(modelo=request.POST ['modelos'], matricula=request.POST ['matricula'])
+
+        ingresar_datos.save()
+
+        return render(request, "entrega/inicio.html")
+    return render(request, "entrega/ingresar_datos.html")
+
+def form_ingresar_datos_camiones(request):
+    if request.method == 'POST':
+        ingresar_datos = Camiones(modelo=request.POST ['modelo'], matricula=request.POST ['matricula'])
+        ingresar_datos.save()
+        return render(request, "entrega/inicio.html")
+    return render(request, "entrega/ingresar_datos.html")
+
+def form_ingresar_datos_motos(request):
+    if request.method == 'POST':
+        ingresar_datos = Motos(modelo=request.POST ['modelo'], matricula=request.POST ['matricula'])
+        ingresar_datos.save()
+        return render(request, "entrega/inicio.html")
+    return render(request, "entrega/ingresar_datos.html")
