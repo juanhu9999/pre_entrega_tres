09b9d4f2fc09a102fb693eb0b3eaab1f640d0c7e
juanhu9999 <juanpablo.isabella@gmail.com>
2024-08-16 15:16:27 -0300

    admin

--- tercera_pre_entrega_isabella/entrega/admin.py
+++ tercera_pre_entrega_isabella/entrega/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Autos, Camiones, Motos
 
-# Register your models here.
+admin.site.register(Autos)
+admin.site.register(Camiones)
+admin.site.register(Motos)
